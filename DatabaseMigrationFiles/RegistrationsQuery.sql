c9c10a3589f465f7fd8aec79eb6010f62a9c39f6
Roy Horizon <royhorizon@reversedhorizonstudio.com>
2025-03-27 03:34:18 +0700

    Added the Query

---
+++ DatabaseMigrationFiles/RegistrationsQuery.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Registrations (
+    Id INT PRIMARY KEY IDENTITY,
+    CompanyName NVARCHAR(255) NOT NULL,
+    NPWP NVARCHAR(50) NOT NULL,
+    DirectorName NVARCHAR(255) NOT NULL,
+    PICName NVARCHAR(255) NOT NULL,
+    Email NVARCHAR(255) NOT NULL,
+    PhoneNumber NVARCHAR(50) NOT NULL,
+    NPWPFilePath NVARCHAR(255),
+    PowerOfAttorneyFilePath NVARCHAR(255)
+);
